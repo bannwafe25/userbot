048a4bb2ff879e3f205ac7e66751128b86515b09
fahlevikun15-dotcom <fahlevikun15@gmail.com>
2026-07-13 09:18:13 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ WAJIB_JOIN = list(
 )
 USENAME_OWNER = os.environ.get("USENAME_OWNER", "sexon12")
 API_ID = int(os.environ.get("API_ID", 36420959))
-MAX_BOT = int(os.environ.get("MAX_BOT", 150))
+MAX_BOT = int(os.environ.get("MAX_BOT", 1000))
 
 API_HASH = os.environ.get("API_HASH", "c28a1377ab0444e6ff89685734f42c12")
 
